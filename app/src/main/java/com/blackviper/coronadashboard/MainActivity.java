4bdc715e81c5490e7c77d0b1a1cc4cf45f05c844
Jan <jannilapp@hotmail.de>
2021-05-02 02:14:02 +0200

    keyboardDown beim Klick auf ein actv-Item und Refactoring

--- app/src/main/java/com/blackviper/coronadashboard/MainActivity.java
+++ app/src/main/java/com/blackviper/coronadashboard/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,7 +21,6 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-import Database.DatabaseHelper;
 import Model.CityDataModel;
 import Model.CityStammdatenModel;
 
@@ -36,29 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
         btn_sendRequest = (Button) findViewById(R.id.btn_sendRequest);
         lv = (ListView) findViewById(R.id.lv_responseView);
-
         tvErgebnisse = (TextView) findViewById(R.id.tvErgebnisse);
-
-        actv_city = (AutoCompleteTextView) findViewById(R.id.actv_Landkreis);
-        List<String> listOfEntries= new ArrayList<String>();
-
-        dataService.getAllCities(new DataService.CityStammdatenResponseListener() {
-            @Override
-            public void onError(String message) {
-                Log.e("ErrGetAllCities", message);
-                showToastTextLong(message);
-            }
-
-            @Override
-            public void onResponse(List<CityStammdatenModel> list) {
-                for(int i = 0; i < list.size(); i++)
-                {
-                    listOfEntries.add(list.get(i).getGen());
-                }
-                actv_city.setAdapter(new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, listOfEntries));
-                showToastTextShort("AutoComplete done.");
-            }
-        });
+        setupActv_city();
+        //setupCloseKeyboardOnTouch(findViewById()); //TODO
 
         btn_sendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                closeKeyboard();
+                keyboardDown();
 
                 dataService.getCityDataByName(userInputCityName, new DataService.CityDataModelResponseListener() {
                     @Override
@@ -91,13 +74,64 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void closeKeyboard()
+    private void setupActv_city()
+    {
+        actv_city = (AutoCompleteTextView) findViewById(R.id.actv_Landkreis);
+        List<String> listOfEntries= new ArrayList<String>();
+
+        actv_city.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                keyboardDown();
+            }
+        });
+
+        dataService.getAllCities(new DataService.CityStammdatenResponseListener() {
+            @Override
+            public void onError(String message) {
+                Log.e("ErrGetAllCities", message);
+                showToastTextLong(message);
+            }
+
+            @Override
+            public void onResponse(List<CityStammdatenModel> list) {
+                for(int i = 0; i < list.size(); i++)
+                {
+                    listOfEntries.add(list.get(i).getGen());
+                }
+                actv_city.setAdapter(new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, listOfEntries));
+                showToastTextShort("AutoComplete done.");
+            }
+        });
+    }
+
+    //TODO Wenns nicht geht, removen
+    //https://stackoverflow.com/questions/4165414/how-to-hide-soft-keyboard-on-android-after-clicking-outside-edittext/11656129
+    public void setupCloseKeyboardOnTouch(View view) {
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    keyboardDown();
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupCloseKeyboardOnTouch(innerView); //Rekursion
+            }
+        }
+    }
+
+    public void keyboardDown()
     {
         View view = this.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
         if (view == null) {
             view = new View(this);
-            //oder man returned direkt
         }
         InputMethodManager imm = (InputMethodManager) this.getSystemService(Activity.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
