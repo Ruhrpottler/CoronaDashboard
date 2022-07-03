9f9ef8b60ab5ce68eded973befb2f4f262711c23
Jan <jannilapp@hotmail.de>
2022-07-03 13:16:53 +0200

    Bugfix: Spalte "BL" existierte noch nicht in lokaler SQLite-DB; Refactoring

--- app/src/main/java/Database/SQLiteDatabaseHelper.java
+++ app/src/main/java/Database/SQLiteDatabaseHelper.java
@@ -16,19 +16,21 @@ import Model.BaseData;
  */
 public class SQLiteDatabaseHelper extends SQLiteOpenHelper
 {
+    private static final String db_name = "basedata.db";
+
     //Tabellen
     private static final String TABLE_CITY_BASE_DATA = "CITY_STAMMDATEN";
     //Spalten
-    private static final String COLUMN_CITY_ID = "OBJECTID";
-    private static final String COLUMN_CITY_BL_ID = "bl_id";
-    private static final String COLUMN_CITY_BL = "bl";
-    private static final String COLUMN_CITY_PRE = "BEZ";
+    private static final String COLUMN_OBJECT_ID = "OBJECTID";
+    private static final String COLUMN_CITY_BL_ID = "BL_ID";
+    private static final String COLUMN_CITY_BL = "BL";
+    private static final String COLUMN_CITY_BEZ = "BEZ";
     private static final String COLUMN_CITY_NAME = "GEN";
     private static final String COLUMN_CITY_EWZ = "EWZ";
 
     public SQLiteDatabaseHelper(@Nullable Context activityContext) //, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version
     {
-        super(activityContext, "coronadata.db", null, 1);
+        super(activityContext, db_name, null, 1);
     }
 
     /**
@@ -39,13 +41,12 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        String createTableStatement = "CREATE TABLE " + TABLE_CITY_BASE_DATA + " (" +
-                "OBJECTID INTEGER NOT NULL PRIMARY KEY, " +
-                "BL_ID INTEGER NOT NULL, " +
-                "BL TEXT NOT NULL, " +
-                "BEZ TEXT NOT NULL, " +
-                "GEN TEXT NOT NULL, " +
-                "EWZ INTEGER " +
+        String createTableStatement = "CREATE TABLE " + TABLE_CITY_BASE_DATA + " (" + COLUMN_OBJECT_ID + " INTEGER NOT NULL PRIMARY KEY, " +
+                COLUMN_CITY_BL_ID + " INTEGER NOT NULL, " +
+                COLUMN_CITY_BL + " TEXT NOT NULL, " +
+                COLUMN_CITY_BEZ + " TEXT NOT NULL, " +
+                COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                COLUMN_CITY_EWZ + " INTEGER " +
                 ")";
 
         db.execSQL(createTableStatement);
@@ -67,36 +68,47 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper
 
     /**
      * Auto-Inkrement Werte (meist PK) müssen dank der ContentValues nicht hinzugefügt werden.
-     * @param model
+     * @param baseData
      * @return Gibt an, ob die Ausführung erfolgreich war
      */
-    public boolean insertOrUpdateCityBaseDataRow(BaseData model)
+    public boolean insertOrUpdateCityBaseDataRow(BaseData baseData)
     {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv = new ContentValues(); //Wie Hashmaps, man kann jz Paare reinpacken (put)
-
-        cv.put(COLUMN_CITY_ID, model.getObjectId());
-        cv.put(COLUMN_CITY_BL_ID, model.getBl_id());
-        cv.put(COLUMN_CITY_BL, model.getBl());
-        cv.put(COLUMN_CITY_PRE, model.getBez());
-        cv.put(COLUMN_CITY_NAME, model.getGen());
-        cv.put(COLUMN_CITY_EWZ, model.getEwz());
-
+        ContentValues cv = fillMap(baseData);
         long success = -1;
 
         try
         {
+            Log.d("SQLite", "Try inserting object '" + baseData.getObjectId() +  "' into table " + TABLE_CITY_BASE_DATA);
             success = db.insertWithOnConflict(TABLE_CITY_BASE_DATA, null, cv, SQLiteDatabase.CONFLICT_IGNORE);
-            if (success == -1)
-                success = db.update(TABLE_CITY_BASE_DATA, cv, "OBJECTID = ?", new String[]{String.valueOf(model.getObjectId())});
+            if (success == -1) //Failed
+            {
+                Log.d("SQLite", "Try updating table " + TABLE_CITY_BASE_DATA + " after insert failed");
+                success = db.update(TABLE_CITY_BASE_DATA, cv, COLUMN_OBJECT_ID + " = ?", new String[]{String.valueOf(baseData.getObjectId())});
+            }
         } catch(Exception e)
         {
-            String msg = String.format("Fehler beim InsertOrUpdate (SQLite) von '%s': \n %s ", model.getCityName(), e.toString());
-            Log.e("DbErr", msg);
+            String msg = String.format("Fehler beim InsertOrUpdate (SQLite) von '%s': \n %s ", baseData.getCityName(), e.toString());
+            Log.e("SQLite", msg);
         } finally
         {
+            Log.d("SQLite", "Successfully inserted or updated data (into) table " + TABLE_CITY_BASE_DATA);
             db.close();
         }
         return (success != -1);
+    }
+
+    private ContentValues fillMap(BaseData baseData)
+    {
+        ContentValues cv = new ContentValues(); //Wie Hashmaps, man kann jz Paare reinpacken (put)
+
+        cv.put(COLUMN_OBJECT_ID, baseData.getObjectId());
+        cv.put(COLUMN_CITY_BL_ID, baseData.getBl_id());
+        cv.put(COLUMN_CITY_BL, baseData.getBl());
+        cv.put(COLUMN_CITY_BEZ, baseData.getBez());
+        cv.put(COLUMN_CITY_NAME, baseData.getGen());
+        cv.put(COLUMN_CITY_EWZ, baseData.getEwz());
+
+        return cv;
     }
 }
