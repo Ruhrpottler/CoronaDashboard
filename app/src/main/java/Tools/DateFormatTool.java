ddd7959c9c9dd53a66e8e9fce52cf2a6c3001cf8
Jan <jannilapp@hotmail.de>
2022-07-26 20:29:42 +0200

    DateFormatTool überarbeitet

--- app/src/main/java/Tools/DateFormatTool.java
+++ app/src/main/java/Tools/DateFormatTool.java
@@ -8,54 +8,65 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.ResolverStyle;
 import java.time.temporal.TemporalAccessor;
+import java.util.Locale;
 
 public class DateFormatTool
 {
-    private static final String FORMAT_GER = "dd.MM.yyyy";
+    protected static final String FORMAT_GER = "dd.MM.yyyy";
     private static final String FORMAT_SORT = "yyMMdd";
-    //private boolean validate
+
+    public static String getFormatGerman()
+    {
+        return FORMAT_GER;
+    }
+
+    public static String getFormatSort()
+    {
+        return FORMAT_SORT;
+    }
 
     /**
      * Validates the input-format and converts it.
      * @param input format dd.MM.yyyy
      * @return format yyMMdd
      */
-    public static String validateAndConvertDateGermanToSortFormat(@NonNull String input) //TODO UnitTests schreiben
+    public static String germanToSort(@NonNull String input)
     {
         return validateAndConvert(input, FORMAT_GER, FORMAT_SORT);
     }
 
     /**
-     * Validates the input-format and converts it.
+     * Validates the input-format and converts it. No validation if the date is really valid (e.g. 31.02.2022)
      * @param input format yyMMdd
      * @return format dd.MM.yyyy
      */
-    public static String validateAndConvertDateSortFormatToGerman(@NonNull String input)
+    public static String sortToGerman(@NonNull String input)
     {
         return validateAndConvert(input, FORMAT_SORT, FORMAT_GER);
     }
 
     private static String validateAndConvert(String input, String formatFrom, String formatTo)
     {
-        if(input == null && input.isEmpty())
+        if(input == null || input.trim().isEmpty())
         {
             throw new IllegalArgumentException("String input is null or empty.");
         }
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatFrom)
+        input = input.trim();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatFrom, Locale.GERMAN)
                 .withResolverStyle(ResolverStyle.STRICT);
         String result;
         try
         {
             TemporalAccessor ta = formatter.parse(input);
-            formatter = DateTimeFormatter.ofPattern(formatFrom)
+            formatter = DateTimeFormatter.ofPattern(formatTo, Locale.GERMAN)
                     .withResolverStyle(ResolverStyle.STRICT);
             result = formatter.format(ta);
         }
         catch(DateTimeParseException e)
         {
-            Log.e(DateFormatTool.class.toString(),
-                    String.format("The date '%s' has an invalid date format, should be '%s'.", input, formatFrom));
-            return input; //TODO Exception Handling
+            String msg = String.format("The date '%s' has an invalid date format, should be '%s'.", input, formatFrom);
+            Log.e(DateFormatTool.class.toString(), msg + "\n" + e.getStackTrace().toString());
+            throw new IllegalArgumentException(msg);
         }
 
         Log.d(DateFormatTool.class.toString(),
